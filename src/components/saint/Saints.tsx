b93bcd96558a475e733e2012b395abf51358879f
Jay Faust <jay.faust@ephesoft.com>
2022-05-02 19:46:36 -0400

    Layout fix.

--- src/components/saint/Saints.tsx
+++ src/components/saint/Saints.tsx
@@ -18,7 +18,7 @@ const Saints: React.FC<{}> = () => {
     const [saints, setSaints] = useState<Array<Saint>>([]);
     useEffect(
         () => {
-            setSaints(getSaintsService.status === 'loaded' ? getSaintsService.payload.filter((saint) => saint.active) : []);
+            setSaints(getSaintsService.status === 'loaded' ? getSaintsService.payload : []);
         },
         [getSaintsService]
     );
@@ -44,46 +44,66 @@ const Saints: React.FC<{}> = () => {
                 </div>
                 {getSaintsService.status === 'loaded' &&
                     <MaterialTable
-                        title=''
                         icons={tableIcons}
-                        options={{actionsColumnIndex: -1}}
-                        localization={{
-                            header: {
-                              actions: ''
+                        options={
+                            {
+                                actionsColumnIndex: -1,
+                                search: false,
+                                showTitle: false,
+                                pageSize: 25
                             }
-                          }}
-                        columns={[
+                        }
+                        localization={
                             {
-                                title: '',
-                                render: (saint) =>  <div className='avatar-container'>
-                                                        <SaintAvatar { ...saint }/>
-                                                    </div>
-                            },
-                            {
-                                title: '',
-                                render: (saint) =>  <div>
-                                                        <h5>{`${saint.name} of ${enumValueToFriendlyName(Region, saint.region! as unknown as object)}`}</h5>
-                                                        <p>{`${saint.yearOfBirth} - ${saint.yearOfDeath}${saint.martyred ? ' (Martyred)' : ''}`}</p>
-                                                    </div>
-                            }
-                        ]}
-                        data={saints}
-                        actions={[
-                            {
-                                icon: tableIcons.Edit as SvgIconComponent,
-                                tooltip: 'Edit',
-                                onClick: (event, saint) => navigate(`/saint/${(saint as Saint).id}`)
-                            },
-                            {
-                                icon: tableIcons.Delete as SvgIconComponent,
-                                tooltip: 'Delete',
-                                onClick: async (event, saint) => {
-                                    await saveSaint({ ...(saint as Saint), active: false });
-
-                                    setSaints([...saints].filter((_) => _.id !== (saint as Saint).id));
+                                header: {
+                                    actions: ''
                                 }
                             }
-                          ]}
+                        }
+                        columns={
+                            [
+                                {
+                                    title: '',
+                                    render: (saint) =>  <div>
+                                                            <div className='avatar-container'>
+                                                                <SaintAvatar { ...saint }/>
+                                                            </div>
+                                                            <h5>{`${saint.name} of ${enumValueToFriendlyName(Region, saint.region! as unknown as object)}`}</h5>
+                                                            <p>{`${saint.yearOfBirth} - ${saint.yearOfDeath}${saint.martyred ? ' (Martyred)' : ''}`}</p>
+                                                        </div>
+                                                        
+                                },
+                                {
+                                    title: '',
+                                    render: (saint) =>  <div>
+                                                            <p>{saint.notes ?? ''}</p>
+                                                        </div>
+                                }
+                            ]
+                        }
+                        data={saints.filter((saint) => saint.active)}
+                        actions={
+                            [
+                                {
+                                    icon: tableIcons.Edit as SvgIconComponent,
+                                    tooltip: 'Edit',
+                                    onClick: (event, saint) => navigate(`/saint/${(saint as Saint).id}`)
+                                },
+                                {
+                                    icon: tableIcons.Delete as SvgIconComponent,
+                                    tooltip: 'Delete',
+                                    onClick: async (event, data) => {
+                                        const saint = data as Saint;
+
+                                        saint.active = false;
+
+                                        await saveSaint(saint);
+
+                                        // setSaints([...saints].filter((_) => _.id !== saint.id));
+                                    }
+                                }
+                            ]
+                        }
                     />
                 }
             </div>
