123dcc5cecb9d8c88fb186a8c01379b60cd914de
Jay Faust <jay.faust@ephesoft.com>
2022-05-10 14:34:38 -0400

    Created UserContext.

---
+++ src/models/security/UserContext.ts
@@ -0,0 +1,19 @@
+export interface UserContext {
+    auth: {
+        access_token: string;
+        id_token: string;
+        login_hint: string;
+        scope: string;
+        expires_in: number;
+        first_issued_at: number;
+        expires_at: number;
+    };
+    userData: {
+        googleId: string;
+        imageUrl: string;
+        email: string;
+        name: string;
+        givenName: string;
+        familyName: string;
+    };
+}
