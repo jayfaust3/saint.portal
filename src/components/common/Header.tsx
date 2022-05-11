272aa0293fe1f6423696b4c52f358ae8df081f5d
Jay Faust <jay.faust@ephesoft.com>
2022-05-10 21:25:45 -0400

    Format cleanup.

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -26,53 +26,56 @@ const Header: FC<unknown> = () => {
     const googleClientId: string = '593080116652-b3nl1jjpf7ke5p294p0atco72eu8dflk.apps.googleusercontent.com';
     
     return (
+      <>
         <AppBar position='fixed'>
-        <Toolbar>
-          {/*Inside the IconButton, we 
-           can render various icons*/}
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            {/*This is a simple Menu 
-             Icon wrapped in Icon */}
-            {/* <MenuIcon /> */}
-          </IconButton>
-          {
-          !isLoggedIn ?
-          <GoogleLogin
-            clientId={googleClientId}
-            buttonText='Login'
-            onSuccess={(response) => {
-                cacheService.setItem(SessionStorageKey.USER_DATA, (response as GoogleLoginResponse));
-                setIsLoggedIn(true);
-                window.location.href = '/';
+          <Toolbar>
+            {/*Inside the IconButton, we 
+            can render various icons*/}
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              {/*This is a simple Menu 
+              Icon wrapped in Icon */}
+              {/* <MenuIcon /> */}
+            </IconButton>
+            {
+            !isLoggedIn ?
+            <GoogleLogin
+              clientId={googleClientId}
+              buttonText='Login'
+              onSuccess={(response) => {
+                  cacheService.setItem(SessionStorageKey.USER_DATA, (response as GoogleLoginResponse));
+                  setIsLoggedIn(true);
+                  window.location.href = '/';
+                }
               }
-            }
             onFailure={(response) => console.error('UNABLE TO LOGIN!!!', JSON.stringify(response))}
-          />
-          :
-          <GoogleLogout
-            clientId={googleClientId}
-            buttonText='Logout'
-            onLogoutSuccess={() => {
-                cacheService.removeItem(SessionStorageKey.USER_DATA);
-                setIsLoggedIn(false);
-                window.location.href = '/';
+            />
+            :
+            <GoogleLogout
+              clientId={googleClientId}
+              buttonText='Logout'
+              onLogoutSuccess={() => {
+                  cacheService.removeItem(SessionStorageKey.USER_DATA);
+                  setIsLoggedIn(false);
+                  window.location.href = '/';
+                }
               }
+              onFailure={() => console.error('UNABLE TO LOGOUT!!!')}
+            />
             }
-            onFailure={() => console.error('UNABLE TO LOGOUT!!!')}
-          />
-          }
-          <Typography variant="h6" 
-            component="div" sx={{ flexGrow: 1 }}>
-            {/* <Text goes here> */}
-          </Typography>
-        </Toolbar>
-      </AppBar>
+            <Typography variant="h6" 
+              component="div" sx={{ flexGrow: 1 }}>
+              {/* <Text goes here> */}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <br></br>
+      </>
     );
 }
 
