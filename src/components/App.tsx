5d87c88b015acd5a282caa0554f212d2b50c6c59
Jay Faust <jay.faust@ephesoft.com>
2022-05-13 15:31:45 -0400

    More cleanup.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,36 +7,36 @@ import AppRouter from './AppRouter';
 import Header from './common/Header';
 
 const App: FC<unknown> = () =>  {
-  let userContext: UserContext = {
-    isLoggedIn: false,
-    auth: {
+    let userContext: UserContext = {
+        isLoggedIn: false,
+        auth: {
         id_token: '',
         expires_in: 3599,
         expires_at: new Date().getTime() + (60 * 60 * 1000)
-    }
-};
-
-const cacheService = new SessionStorageService();
-
-const userData: GoogleLoginResponse | null = cacheService.getItem(SessionStorageKey.USER_DATA, false);
-
-if (userData) {
-    userContext = {
-        isLoggedIn: true,
-        auth: userData.tokenObj,
-        userData: userData.profileObj
+        }
     };
-}
-  return (
-    <div className="App">
-      <div className="container">
-        <Header userContext={userContext}/>
-        <Router>                                      
-          <AppRouter userContext={userContext}/>
-        </Router>
-      </div>
-    </div>
-  );
+
+    const cacheService = new SessionStorageService();
+
+    const userData: GoogleLoginResponse | null = cacheService.getItem(SessionStorageKey.USER_DATA, false);
+
+    if (userData) {
+        userContext = {
+            isLoggedIn: true,
+            auth: userData.tokenObj,
+            userData: userData.profileObj
+        };
+    }
+    return (
+        <div className="App">
+            <div className="container">
+                <Header userContext={userContext}/>
+                <Router>                                      
+                    <AppRouter userContext={userContext}/>
+                </Router>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -7,33 +7,12 @@ import Saints from './saint/Saints';
 import Saint from './saint/Saint';
 
 const AppRouter: FC<{ userContext: UserContext }> = (props: PropsWithChildren<{ userContext: UserContext }>) => {
-    let userContext: UserContext = {
-        isLoggedIn: false,
-        auth: {
-            id_token: '',
-            expires_in: 3599,
-            expires_at: new Date().getTime() + (60 * 60 * 1000)
-        }
-    };
-
-    const cacheService = new SessionStorageService();
-
-    const userData: GoogleLoginResponse | null = cacheService.getItem(SessionStorageKey.USER_DATA, false);
-
-    if (userData) {
-        userContext = {
-            isLoggedIn: true,
-            auth: userData.tokenObj,
-            userData: userData.profileObj
-        };
-    }
-
     return (
         <div>             
             <Routes>
-              <Route path='/' element={<Saints userContext={userContext}/>} /> 
-              <Route path='/saint' element={<Saint userContext={userContext}/>} />                      
-              <Route path='/saint/:saintId' element={<Saint userContext={userContext}/>} />
+              <Route path='/' element={<Saints userContext={props.userContext}/>} /> 
+              <Route path='/saint' element={<Saint userContext={props.userContext}/>} />                      
+              <Route path='/saint/:saintId' element={<Saint userContext={props.userContext}/>} />
             </Routes>               
         </div>
     );
